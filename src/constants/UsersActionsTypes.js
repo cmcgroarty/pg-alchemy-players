50c252b8fff57740ecbf32a5505d03df256f4fdb
Chris McGroarty <iammarix@gmail.com>
2018-06-12 17:28:27 -0400

    add user redux

---
+++ src/constants/UsersActionsTypes.js
@@ -0,0 +1,11 @@
+export const FETCH_USERS = 'GET_USERS';
+export const FETCH_USER = 'GET_USER';
+export const CREATE_USER = 'CREATE_USER';
+export const UPDATE_USER = 'UPDATE_USER';
+export const DELETE_USER = 'DELETE_USER';
+
+export const USER_FETCHED = 'USER_FETCHED';
+export const USERS_FETCHED = 'USERS_FETCHED';
+export const USER_CREATED = 'USER_CREATED';
+export const USER_UPDATED = 'USER_UPDATED';
+export const USER_DELETED = 'USER_DELETED';
---
+++ src/reducers/users.js
@@ -0,0 +1,13 @@
+import * as types from '../constants/UsersActionsTypes';
+
+const initialState = [];
+
+export default function users(state=initialState, action){
+	switch(action.type){
+		case types.USERS_FETCHED:
+			return action.users;
+		case types.USER_CREATED:
+			return [...state, action.user];
+	}
+}
+
