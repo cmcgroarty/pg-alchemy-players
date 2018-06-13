4b21526acace626df0f4968b34edf411d76f9222
Chris McGroarty <iammarix@gmail.com>
2018-06-12 22:33:15 -0400

    dispatch sub-actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,14 +1,13 @@
-import * as types from '../constants/AuthActionTypes';
-import {setError} from './error';
+import * as types from '../constants/AuthActionTypes'
+import {setError} from './error'
 
-export const attemptLogin = credentials => {
-	fetch('', {method: 'POST', body: credentials}).then(response => {
-		if(response.success){
-
-		} else {
-			setError(response.error);
-		}
-	});
-};
-export const logout = () => {{type:types.LOGOUT}};
-export const loginSuccess = response => ({type:types.LOGIN_SUCCESS, user: response.user, token: response.token});
+export const attemptLogin = credentials => dispatch =>
+  fetch('', {method: 'POST', body: credentials}).then(response => {
+    if (response.success) {
+      dispatch(this.loginSuccess(response))
+    } else {
+      dispatch(setError(response.error))
+    }
+  })
+export const logout = () => ({type: types.LOGOUT})
+export const loginSuccess = response => ({type: types.LOGIN_SUCCESS, user: response.user, token: response.token})
