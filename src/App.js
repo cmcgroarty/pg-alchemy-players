a900bcc3ce71549d80f4a73ab9ba48de36ca1e93
Chris McGroarty <iammarix@gmail.com>
2018-06-15 15:48:04 -0400

    add topbar and padding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router'
 
-
 import './App.css'
 import Login from './containers/Login'
 import Players from './containers/Players'
@@ -12,9 +11,20 @@ class App extends Component {
   render () {
     return (
       <div>
+        <div className='top-bar'>
+          <div className='grid-container'>
+            <div className='grid-x'>
+              <div className='top-bar-left'>
+                <ul className='dropdown menu' data-dropdown-menu>
+                  <li className='menu-text'>P&G Alchemy Players API</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
         <CalloutDisplay />
         <div className='grid-container'>
-          <div className='grid-x'>
+          <div className='grid-x grid-padding-x grid-padding-y'>
             <div className='cell'>
               <Switch>
                 <Route path='/user' component={User} />
