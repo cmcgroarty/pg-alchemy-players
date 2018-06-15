9dd1dc1970aefd988f32f0ac1f8020d046d16973
Chris McGroarty <iammarix@gmail.com>
2018-06-15 15:19:22 -0400

    add redux thunk/logger middleware & BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import { Provider } from 'react-redux'
+import ReduxThunk from 'redux-thunk'
+import logger from 'redux-logger'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css'
+import App from './App'
+import reducer from './reducers'
+
+const store = createStore(
+  reducer,
+  applyMiddleware(
+    ReduxThunk,
+    logger
+  )
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+)
